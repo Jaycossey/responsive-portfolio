4c2f2f9e8db821387c2fe95a6e2cb4a7d8d8def9
jaycossey <jaycossey@live.com>
2023-10-18 12:01:31 +0100

    added run animation

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -266,11 +266,76 @@ generateIcons();
 
 // ANIMATION IN BANNER ----------------------------------------------------------------------------------------------------------
 
-// // This is for a future update. Currently the banner class will only display text / gradient
-
 // Khayman Animation
+// Create reference to canvas and context
 const playerCanvas = document.getElementById('khayman');
+const playerCtx = playerCanvas.getContext('2d');
 
+// Set default state for sprite
+let playerState = 'run';
+
+// Canvas width and height
+const PLAYER_CANVAS_WIDTH = playerCanvas.width = 32;
+const PLAYER_CANVAS_HEIGHT = playerCanvas.height = 32;
+
+// assign spritesheet
+const playerSprite = new Image();
+playerSprite.src = "./assets/images/khayman-spritesheet.png";
+
+// assign spritesheet frame width and height
+const spriteWidth = 32;
+const spriteHeight = 32;
+
+// declare and assign framerates
+let playerFrame = 0;
+const staggerFrames = 6;
+
+// set animation states and array
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'run',
+        frames: 7
+    },
+    {
+        name: 'jump',
+        frames: 7
+    }
+];
+
+// set frame locations on spritesheet
+animationStates.forEach((state, index) => {
+    // create array for each of the states locations
+    let frames = {
+        loc: [],
+    }
+    // for each of the total frames required, assign the locations
+    for (let i = 0; i < state.frames; i++) {
+        let positionX = i * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    // assign the locations to each state
+    spriteAnimations[state.name] = frames;
+});
+
+// Animate player sprite
+function playerAnimate() {
+    // clear current frame
+    playerCtx.clearRect(0, 0, PLAYER_CANVAS_WIDTH, PLAYER_CANVAS_HEIGHT);
+    // funky maths to assign position of the current sprite frame
+    let position = Math.floor(playerFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+
+    // draw the new frame using the assigned position of each frame
+    playerCtx.drawImage(playerSprite, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+    playerFrame++;
+    requestAnimationFrame(playerAnimate);
+}
+
+playerAnimate();
 
 // Parallax Background Animation
 // Create reference to canvas and context 2d
